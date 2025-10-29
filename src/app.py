f552a02ac34f5539f4b07ddcf671ea241fd7f6ae
Kenneth Tay <kenneth21tay@gmail.com>
2025-10-29 15:42:18 +0800

    Added a way to load trained model and tokeniser.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from transformers import AutoModelForCausalLM, AutoTokenizer
 app = Flask(__name__)
+
+# Load model and tokenizer globally to avoid reloading on every request
+model = AutoModelForCausalLM.from_pretrained("./model/deadpool-gpt2")
+tokenizer = AutoTokenizer.from_pretrained("./model/deadpool-gpt2")
 
 @app.route('/')
 def home():
